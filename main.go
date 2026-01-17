7603bcd703454aae5ce7254e74a8ed668f9ff81e
Bibek Tamang <bibek7here@gmail.com>
2026-01-17 16:14:11 +0000

    init udp connection

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 )
 
 func main() {
-	_, err := net.ResolveUDPAddr("udp", ":3000")
+	fmt.Println("Logs from your program will appear here!")
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	if err != nil {
 		log.Fatal(err)
+		return
+	}
+	udpConn, err := net.ListenUDP("upd", udpAddr)
+	if err != nil {
+		fmt.Println("Failed to bind to addresss: ", err)
+		return
+	}
+
+	defer udpConn.Close()
+
+	buf := make([]byte, 512)
+
+	for {
+		size, source, err := udpConn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error receiving data:", err)
+			break
+		}
+		receivedData := string(buf[:size])
+		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+
+		response := []byte{}
+		_, err = udpConn.WriteToUDP(response, source)
+
+		if err != nil {
+			fmt.Println("Failed to send response: ", err)
+		}
 	}
 }
